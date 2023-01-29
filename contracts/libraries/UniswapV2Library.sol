3655c4a30de9409895ed28cd0adc7b60042271b5
0xober <0xober@proton.me>
2023-01-29 11:55:54 +0000

    usign aegis init hash code

--- contracts/libraries/UniswapV2Library.sol
+++ contracts/libraries/UniswapV2Library.sol
@@ -21,7 +21,7 @@ library UniswapV2Library {
                 hex'ff',
                 factory,
                 keccak256(abi.encodePacked(token0, token1)),
-                hex'96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f' // init code hash
+                hex'f5ee3145a57b59a8c07c91469ad99626d18ce942c87b71e1c7795cb4f9ca7829' // init code hash
             ))));
     }
 
