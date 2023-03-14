c73b024bd306631ffe8d45d8574ea5812a3374f3
0xober <0xober@proton.me>
2023-03-14 15:21:38 +0000

    added external router in test for integration tests

--- deployment/integration-test/test_utils/aegisAmmManager.ts
+++ deployment/integration-test/test_utils/aegisAmmManager.ts
@@ -1,25 +1,32 @@
-import {deployWeth} from '../../scripts/functions';
-import {deployRouter} from '../../scripts/functions';
-import factoryOut  from '../../../../v2-core/deployment/integration-test/out/ammAddresses.json';
+import { deployWeth } from '../../scripts/functions';
+import { deployRouter } from '../../scripts/functions';
+//available in integration test only
+import factoryOut from '../../../../v2-core/deployment/integration-test/out/ammAddresses.json';
 
 export class AmmManager {
 	public address: string;
 	private key: string;
 	public automatedMarketMakerAddress: string;
 	public wrappedEthAddress: string;
+	public testExternalRouter: string;
 
 	constructor(_address: string, _key: string) {
 		this.address = _address;
 		this.key = _key;
 		this.automatedMarketMakerAddress = '';
 		this.wrappedEthAddress = '';
+		this.testExternalRouter = "";
 	}
 
-	public deployWrappedGasToken = async() => {
-		this.wrappedEthAddress = await deployWeth(); 
+	public deployWrappedGasToken = async () => {
+		this.wrappedEthAddress = await deployWeth();
 	}
 
-	public deployAutomatedMarketMaker = async() => {
+	public deployAutomatedMarketMaker = async () => {
 		this.automatedMarketMakerAddress = await deployRouter(factoryOut.factory, this.wrappedEthAddress);
-	} 
+	}
+
+	public deployExternalAutomatedMarketMaker = async () => {
+		this.testExternalRouter = await deployRouter(factoryOut.factory, this.wrappedEthAddress);
+	}
 }
